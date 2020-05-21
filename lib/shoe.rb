a8097718bd41cb548a9d5a3b01b50716773a1374
Thanh Pham <tpham0123@gmail.com>
2020-05-21 17:24:09 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,8 +6,9 @@ class Shoe
   
   def initialize(brand)
     @brand = brand
-    if !(BRANDS.include?(@brand))
-      BRANDS << @brand
+    if BRANDS.include?(brand)
+    else
+      BRANDS << brand
     end
   end
 
